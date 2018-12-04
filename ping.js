a067d4491a086d47b5ca1cc144b28c0f2be25b62
José Rafael Vieira <16804617+jrvieira@users.noreply.github.com>
2018-12-04 19:50:58 +0000

    Update ping.js

--- ping.js
+++ ping.js
@@ -1,15 +1,13 @@
 const ping = require('ping');
 const note = require('node-notifier');
 
-
-
 var engine = setInterval(function () {
-	ping.sys.probe(process.argv[2], function (pong) {
-		if (pong) {
-
-			note.notify('Online!');
-			clearInterval(engine);
-
-		}
-	})
-}, 2000);
+    ping.sys.probe(process.argv[2], function (pong) {
+	    
+        if (pong) {
+            note.notify('Online!');
+            clearInterval(engine);
+        }
+	    
+    })
+}, 500);
